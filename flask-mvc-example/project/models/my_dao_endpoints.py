9e35d9b165d2f77c1aafd7ca64f32894e6de220c
alopsahl <100840753+alopsahl@users.noreply.github.com>
2023-11-05 17:59:32 +0100

    Add files via upload

--- flask-mvc-example/project/models/my_dao_endpoints.py
+++ flask-mvc-example/project/models/my_dao_endpoints.py
@@ -31,7 +31,6 @@ def findAllUsers():
     with get_connection().session() as session:
         users = session.run("MATCH(c:User) RETURN c;")
         nodes_json = [node_to_json(record["c"]) for record in users]
-        # print(type(nodes_json))
         print_json(nodes_json)
         return nodes_json
 
@@ -82,10 +81,7 @@ def update_car(make, model, car_id, year, location, car_state):
         print_json(nodes_json)
         return nodes_json
 
-# TODO Hvorfor .execute_query her istedenfor session.run som på de andre? 
-# As is: TypeError: The view function for 'delete_car_info' did not return a valid response. The function either returned None or ended without a return statement.
 def delete_car(car_id):
-    # get_connection().execute_query("MATCH (c:Cars{car_id: $car_id}) Detach DELETE c", car_id=car_id)
     with get_connection().session() as session: 
         employees = session.run("MATCH (c:Cars{car_id: $car_id}) Detach DELETE c", car_id=car_id)
 
@@ -93,7 +89,6 @@ def delete_car(car_id):
         return print_json(nodes_json)
 
 ### CUSTOMERS - FUNKSJONER ###
-# OK
 def findAllCustomers():
     with get_connection().session() as session:
         customers = session.run("MATCH(c:Customer) RETURN c;")
@@ -102,7 +97,6 @@ def findAllCustomers():
         print_json(nodes_json)
         return nodes_json 
 
-# Virker! 
 def findCustomerByName(name):
     data = get_connection().execute_query("MATCH (a:Customer) where a.name = $name RETURN a;", name=name)
     if len(data[0]) > 0:
@@ -112,7 +106,6 @@ def findCustomerByName(name):
     else:
         return Customer(name, "Not found in DB")
 
-# OK, json
 def findCustomerbyIdNumber(customer_id):
     with get_connection().session() as session:
         customers = session.run("MATCH (c:Customer{customer_id: $customer_id}) return c;", customer_id=customer_id)
@@ -122,7 +115,6 @@ def findCustomerbyIdNumber(customer_id):
         print_json(nodes_json)
         return nodes_json
 
-# OK, json    
 def save_customer(customer_id, name, email, age, address):
     with get_connection().session() as session: 
         customers = session.run("MERGE (c:Customer{customer_id: $customer_id, name: $name, email: $email, age: $age, address: $address}) RETURN c;", 
@@ -132,7 +124,6 @@ def save_customer(customer_id, name, email, age, address):
         print_json(nodes_json)
         return nodes_json
 
-# OK, json    
 def update_customer(customer_id, name, email, age, address):
     with get_connection().session() as session: 
         customers = session.run("MATCH (c:Customer{customer_id: $customer_id}) SET c.customer_id=$customer_id, c.name=$name, c.email=$email, c.age=$age, c.address=$address RETURN c;", 
@@ -142,7 +133,7 @@ def update_customer(customer_id, name, email, age, address):
         print_json(nodes_json)
         return nodes_json
 
-# TODO Virker ikke?!?????????
+
 def delete_customer(customer_id):
     with get_connection().session() as session: 
         customers = session.run("MATCH (c:Customer{customer_id: $customer_id}) DETACH DELETE c;", customer_id=customer_id)
@@ -152,7 +143,6 @@ def delete_customer(customer_id):
         return nodes_json
 
 ### EMPLOYEES ###
-# OK
 def findAllEmployees():
     with get_connection().session() as session:
         employees = session.run("MATCH(c:Employee) RETURN c;")
@@ -162,7 +152,6 @@ def findAllEmployees():
         print_json(nodes_json)
         return nodes_json
 
-# OK
 def findEmployeebyIdNumber(emp_id):
     with get_connection().session() as session:
         employees = session.run("MATCH (c:Employee{emp_id: $emp_id}) return c;", emp_id=emp_id)
@@ -171,7 +160,6 @@ def findEmployeebyIdNumber(emp_id):
         print_json(nodes_json)
         return nodes_json
 
-# OK
 def save_employee(emp_id, name, address, branch):
     with get_connection().session() as session: 
         employees = session.run("MERGE (c:Employee{emp_id: $emp_id, name: $name, address: $address, branch: $branch}) RETURN c;", 
@@ -181,7 +169,6 @@ def save_employee(emp_id, name, address, branch):
         print_json(nodes_json)
         return nodes_json
 
-# OK     
 def update_employee(emp_id, name, address, branch):
     with get_connection().session() as session: 
         employees = session.run("MATCH (c:Employee{emp_id: $emp_id}) SET c.emp_id=$emp_id, c.name=$name, c.address=$address, c.branch=$branch RETURN c;", 
@@ -191,7 +178,6 @@ def update_employee(emp_id, name, address, branch):
         print_json(nodes_json)
         return nodes_json
         
-# OK
 def delete_employee(emp_id, name, address, branch):
     with get_connection().session() as session: 
         employees = session.run("MATCH (c:Employee{emp_id: $emp_id}) DETACH DELETE c;", emp_id=emp_id, name=name, address=address, branch=branch)
@@ -202,9 +188,6 @@ def delete_employee(emp_id, name, address, branch):
 
 ### ACTIONS ###
 
-
-
-#FUNKSJONER TIL ORDERS#
 
 #sjekk om bil er tilgjengelig
 def check_availability(car_id):
@@ -221,7 +204,7 @@ def check_eligibility(customer_id):
 def generate_order_id():
     return(random.randint(100000,999999))
 
-#sjekk om det finnes en ordre med den kunden og den bilen - returnerer en streng, siden vi ikke får lov til å returnere bl.a. bool og int 
+#sjekk om det finnes en ordre med den kunden og den bilen 
 def order_exists(car_id,customer_id):
     with get_connection().session() as session: 
         order = session.run("MATCH (n:Order) WHERE n.customer_id = $customer_id AND n.car_id= $car_id RETURN n",customer_id=customer_id,car_id=car_id)
@@ -246,6 +229,7 @@ def make_order(car_id,customer_id):
                                 order_id=order_id,customer_id=customer_id,car_id=car_id )
             nodes_json = ([node_to_json(record["c"]) for record in order])
             session.run("MATCH (a:Cars{car_id: $car_id}) SET a.car_state='booked'",car_id=car_id)
+            session.run("MATCH (c:Customer{customer_id: $customer_id}) SET c.is_renting='yes'",customer_id=customer_id)
             return(nodes_json) 
     else:
         return ("Terribly sorry, but conditions aren't met")
@@ -262,12 +246,11 @@ def findAllOrders():
 
 # CANCEL ORDER CAR
 
-#må taste inn både bilnummer og kundenummer, siden det er det oppgaven ber om. Funker nå
+#må taste inn både bilnummer og kundenummer, siden det er det oppgaven ber om.
 def cancel_order_car(car_id,customer_id):
     order = order_exists(car_id,customer_id)
     if len(order)>0:
-        order_id = order[0]["order_id"]
-        
+        order_id = order[0]["order_id"]     
         
         with get_connection().session() as session:
             session.run("MATCH (o:Order {order_id: $order_id}) DELETE o;",order_id=order_id)
@@ -281,57 +264,42 @@ def cancel_order_car(car_id,customer_id):
 # RENT CAR
 
 def rent_car(car_id,customer_id):
-
-    return()
+    order = order_exists(car_id,customer_id)
+    if len(order)>0:
+        order_id = order[0]["order_id"]        
+        
+        with get_connection().session() as session:
+            session.run("MATCH (a:Cars{car_id: $car_id}) SET a.car_state='rented'",car_id=car_id)
+            return("car is rented")
+    else:
+        return("no such order")
 
 # RETURN CAR
 
-def return_car(order_id):
-    return()
+def return_car(car_id,customer_id,car_state):
+    order = order_exists(car_id,customer_id)
+    if len(order)>0:
+        order_id = order[0]["order_id"]     
+        print(car_state)
+        with get_connection().session() as session:
+            session.run("MATCH (a:Cars{car_id: $car_id}) SET a.car_state= $car_state",car_id=car_id,car_state=car_state)
+            session.run("MATCH (c:Customer{customer_id: $customer_id}) SET c.is_renting='no'",customer_id=customer_id)
+            session.run("MATCH (o:Order {order_id: $order_id}) DELETE o;",order_id=order_id)
 
-
+            print("car returned")
+            return("car returned")
+    else:
+        return("no such order")
+    
 #HJELPEFUNKSJONER
 
 #gjør alle biler tilgjengelige
 
+#sett alle 
 
 
-#opprett relasjon 
-def make_relation():
-    car_id=22
-    customer_id=5000
-    with get_connection().session() as session:
-        session.run("MATCH (Cars {car_id: 4}), (Customer {customer_id: 60}) CREATE (Customer)-[:KRÆSJET]->(Cars)") 
-        return 'relasjon opprettet'
 
-#hent relasjoner (funkar inte)
-def findAllRelations():
-    print('første')
-    with get_connection().session() as session:
-        print('andre')
-        relations = session.run("MATCH (a)-[r]-(c) RETURN *")
-        print("RELATIONS TYPE:\n",type(relations))
-        for record in relations:
-            print('tredje')
-            print(type(record)) 
-            print(record)
-            print(record[0])
-            for i in record:
-                print("ting:",i)
 
-        nodes_json = [node_to_json(record[0]) for record in relations]
-        print_json(nodes_json)
-        return nodes_json
-
-#sjekk om kunde inngår i relasjon
-def check_relation():
-    with get_connection().session() as session:
-        session.run("MATCH (Cars {car_id: 4}), (Customer {customer_id: 60}) CREATE (Customer)-[:KRÆSJET]->(Cars)") 
-
-#slett relasjon
-def delete_relation():
-        with get_connection().session() as session:
-            session.run("")
 
 
 
